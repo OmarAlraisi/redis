ca5f7848d3319b3321f073c004a856164ff9be89
AlRaisi96 <o.raisi@hotmail.com>
2024-01-10 15:02:29 +0400

    finally solves the connection closed error

--- src/main.rs
+++ src/main.rs
@@ -20,25 +20,43 @@ fn main() -> std::io::Result<()> {
             std::borrow::Cow::Owned(str) => str,
         };
 
-        match Message::new(message_input) {
-            Err(err) => {
-                println!("{}", err);
-                let new_string = format!("-{}", err);
-                stream.write(new_string.as_bytes()).unwrap();
-            }
-            Ok(message) => match message {
-                Message::BulkString(message) => {
-                    println!("{}", message);
-                    stream.write(message.as_bytes()).unwrap();
-                }
-                _ => {}
+        let message_output = match Command::parse_command(message_input) {
+            Ok(command) => {
+                println!("{}", command.command);
+                println!("{:?}", command.args);
+                command.command
             },
-        }
+            Err(err) => err,
+        };
 
-        stream.write(b"+PONG\r\n").unwrap();
+        stream
+            .write(format!("+{}\r\n", message_output).as_bytes())
+            .unwrap();
 
         stream.shutdown(Shutdown::Both).unwrap();
     }
 
     Ok(())
+}
+
+struct Command {
+    command: String,
+    args: Vec<Message>,
+}
+
+impl Command {
+    fn parse_command(raw_message: String) -> Result<Self, String> {
+        if let Ok(Message::Array(message)) = Message::new(raw_message) {
+            let mut tokens = message.into_iter();
+            Ok(Command {
+                command: match tokens.next().unwrap() {
+                    Message::BulkString(command) => command,
+                    _ => return Err(String::from("Invalid message!")),
+                },
+                args: tokens.collect::<Vec<Message>>(),
+            })
+        } else {
+            Err(String::from("-Invalid\r\n"))
+        }
+    }
 }
