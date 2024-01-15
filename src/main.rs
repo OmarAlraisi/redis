07d8273457bb668c9d4ec50a25cb5454ae4e0f9b
AlRaisi96 <o.raisi@hotmail.com>
2024-01-15 13:24:12 +0400

    multithreaded

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ mod resp_data;
 
 use std::{
     io::{Read, Write},
-    net::{Shutdown, TcpListener},
+    net::TcpListener,
+    sync::{Arc, Mutex},
+    thread,
 };
 
 use db::DB;
@@ -12,18 +14,26 @@ use resp::RESP;
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379")?;
-    let mut db = DB::new();
+    let db = Arc::new(Mutex::new(DB::new()));
 
     for stream in listener.incoming() {
         let mut stream = stream?;
-        let mut buffer = [0; 1024 * 1024];
-        let num_of_bytes = stream.read(&mut buffer).unwrap();
-        let input_message = RESP::deserialize(&buffer[..num_of_bytes]);
-        let output_message = db.proccess_message(input_message);
+        let db = Arc::clone(&db);
 
-        stream.write(&RESP::serialize(output_message)).unwrap();
+        thread::spawn(move || loop {
+            let mut buffer = [0; 1024 * 1024];
+            let num_of_bytes = match stream.read(&mut buffer) {
+                Ok(num_of_bytes) => num_of_bytes,
+                Err(_) => break,
+            };
+            let input_message = RESP::deserialize(&buffer[..num_of_bytes]);
+            let output_message = db.lock().unwrap().proccess_message(input_message);
 
-        stream.shutdown(Shutdown::Both).unwrap();
+            match stream.write(&RESP::serialize(output_message)) {
+                Ok(_) => {}
+                Err(_) => continue,
+            };
+        });
     }
 
     Ok(())
